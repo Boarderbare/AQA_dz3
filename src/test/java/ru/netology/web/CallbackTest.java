18791bb68fb1cc84361aa160c2b6fe3a9b1db9fd
boarderbare <boarderbare@gmail.com>
2022-07-25 18:31:58 +0400

    initial

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,71 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void testForm() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван");
+        elements.get(1).sendKeys("+78884547878");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+
+    @Test
+    void testFormCss() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+78884547878");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+//    @Test
+//    class CallbackTest {
+//        @Test
+//        void shouldSubmitRequest() {
+//            open("http://localhost:9999");
+//            SelenideElement form = $("[data-test-id=callback-form]");
+//            form.$("[data-test-id=name] input").setValue("Василий");
+//            form.$("[data-test-id=phone] input").setValue("+79270000000");
+//            form.$("[data-test-id=agreement]").click();
+//            form.$("[data-test-id=submit]").click();
+//            $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+//        }
+//    }
+}
