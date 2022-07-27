6cd61278d4d29d55bd9c83302ffad4b2950f018e
boarderbare <boarderbare@gmail.com>
2022-07-27 15:45:36 +0400

    project on selendine

--- src/test/java/ru/netology/web/FormTest.java
+++ src/test/java/ru/netology/web/FormTest.java
@@ -1,21 +1,16 @@
 package ru.netology.web;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WrapsElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormTest {
     private WebDriver driver;
@@ -23,18 +18,13 @@ public class FormTest {
 
     @BeforeAll
     static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
 
-//        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -44,73 +34,56 @@ public class FormTest {
     }
 
     @Test
-    void testForm() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иван");
-        elements.get(1).sendKeys("+78884547878");
-        driver.findElement(By.className("checkbox")).click();
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $(".paragraph").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void testFormCss() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+78884547878");
-        driver.findElement(By.className("checkbox")).click();
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    void shouldReturnIncorrectFieldName() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("sfdf");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldReturnIncorrectFieldPhone() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
-    void testFormFieldsNameIncorrect() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("dghdg");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+78884547878");
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText().trim();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    void shouldReturnEmptyFieldName() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
+
     @Test
-    void testFormFieldsNameEmpty() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+78884547878");
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText().trim();
-        assertEquals("Поле обязательно для заполнения", text);
+    void shouldReturnEmptyFielddPhone() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
-    @Test
-    void testFormFieldPhoneIncorrect() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("545454");
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub")).getText().trim();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
-    }
-    @Test
-    void testFormFieldPhoneEmpty() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub")).getText().trim();
-        assertEquals("Поле обязательно для заполнения", text);
-    }
-//    @Test
-//    class CallbackTest {
-//        @Test
-//        void shouldSubmitRequest() {
-//            open("http://localhost:9999");
-//            SelenideElement form = $("[data-test-id=callback-form]");
-//            form.$("[data-test-id=name] input").setValue("Василий");
-//            form.$("[data-test-id=phone] input").setValue("+79270000000");
-//            form.$("[data-test-id=agreement]").click();
-//            form.$("[data-test-id=submit]").click();
-//            $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-//        }
-//    }
 }
